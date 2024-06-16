ddb97f84a7b9df78b59d465b2adf4d45712be8aa
Leigh Allen <lallenation@gmail.com>
2024-06-16 21:01:53 +0000

    feat: add teams and tasks to admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Task, Category, TaskFile
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task_name', 'is_urgent', 'due_date', 'category', 'team')
+    list_filter = ('is_urgent', 'due_date', 'category', 'team')
+    search_fields = ('task_name', 'description')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(TaskFile)
+class TaskFileAdmin(admin.ModelAdmin):
+    list_display = ('file', 'uploaded_at')
+    search_fields = ('file',)
