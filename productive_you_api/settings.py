46db166b31be8c7df4d132cd8681221ae4897bf1
Leigh Allen <lallenation@gmail.com>
2024-06-18 22:06:36 +0000

    fix: add allowed hosts to settings

--- productive_you_api/settings.py
+++ productive_you_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-leighallend-productivey-g0aq403wfsg.ws.codeinstitute-ide.net', 'productive_you_api.herokuapp.com']
+ALLOWED_HOSTS = ['8000-leighallend-productivey-g0aq403wfsg.ws.codeinstitute-ide.net', '*.herokuapp.com']
 
 
 # Application definition
