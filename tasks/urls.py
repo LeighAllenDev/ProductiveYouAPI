e0236b1a32acc08a313ffbafad5e354776a06d12
Leigh Allen <lallenation@gmail.com>
2024-06-25 23:57:57 +0000

    fix: edit tasklist urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import TaskListCreateView, TaskDetailView, CategoryListCreateView, CategoryDetailView
 
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='task')
+router.register(r'categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
-    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
-    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
-    path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
-    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+    path('api/tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('api/tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+    path('api/categories/', CategoryListCreateView.as_view(), name='category-list-create'),
+    path('api/categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+    path('api/', include(router.urls)),
 ]
