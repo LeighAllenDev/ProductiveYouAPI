3dc468e5cbcc3a43dd4be8a31a5da204f89dfab9
Leigh Allen <lallenation@gmail.com>
2024-06-26 00:07:22 +0000

    fix: remove default router from api urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
 from .views import TaskListCreateView, TaskDetailView, CategoryListCreateView, CategoryDetailView
 
-router = DefaultRouter()
-router.register(r'tasks', TaskViewSet, basename='task')
-router.register(r'categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
-    path('api/tasks/', TaskListCreateView.as_view(), name='task-list-create'),
-    path('api/tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
-    path('api/categories/', CategoryListCreateView.as_view(), name='category-list-create'),
-    path('api/categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
-    path('api/', include(router.urls)),
+    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+    path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
 ]
