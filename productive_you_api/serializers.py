44d7797ad5e29d139002c4ae7b00b3e6a91ca91a
Leigh Allen <lallenation@gmail.com>
2024-06-29 11:15:01 +0000

    edit: change app serializer for current user

--- productive_you_api/serializers.py
+++ productive_you_api/serializers.py
@@ -1,8 +1,15 @@
-# In your serializers.py
 from dj_rest_auth.serializers import UserDetailsSerializer
 from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('id', 'name', 'content', 'image')
 
 class CurrentUserSerializer(UserDetailsSerializer):
+    profile = ProfileSerializer(read_only=True)
+
     class Meta(UserDetailsSerializer.Meta):
         model = User
-        fields = ('pk', 'username', 'email', 'first_name', 'last_name')
+        fields = ('pk', 'username', 'profile')
