be3c069df7a62be23e5d6f2e43418790dfcae7a3
Leigh Allen <lallenation@gmail.com>
2024-06-30 22:46:48 +0000

    fix: change team members to team users

--- teams/models.py
+++ teams/models.py
@@ -5,7 +5,7 @@ from profiles.models import Profile
 class Team(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    members = models.ManyToManyField(Profile, related_name='teams', blank=True)
+    users = models.ManyToManyField(Profile, related_name='teams', blank=True)
 
     def __str__(self):
         return self.name
--- teams/serializers.py
+++ teams/serializers.py
@@ -3,11 +3,8 @@ from .models import Team
 from profiles.models import Profile
 
 class TeamSerializer(serializers.ModelSerializer):
-    members = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    member_profiles = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Profile.objects.all(), write_only=True, source='members'
-    )
+    users = serializers.PrimaryKeyRelatedField(many=True, queryset=Profile.objects.all(), write_only=True, source='users')
 
     class Meta:
         model = Team
-        fields = ['id', 'name', 'members', 'member_profiles']
+        fields = ['id', 'name', 'users']
