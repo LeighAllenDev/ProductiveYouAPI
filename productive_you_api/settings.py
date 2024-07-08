d5c2685720404dec77c396750fd50a6e1bbaf25a
Leigh Allen <lallenation@gmail.com>
2024-07-08 20:55:29 +0000

    add cookies

--- productive_you_api/settings.py
+++ productive_you_api/settings.py
@@ -92,6 +92,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_COOKIE_SECURE = True  # Ensure CSRF cookie is only sent over HTTPS
+CSRF_COOKIE_SAMESITE = 'None'  # Allow CSRF cookie to be sent with cross-site requests
+CSRF_COOKIE_DOMAIN = 'https://3000-leighallend-reactproduc-xjtynup7n1l.ws.codeinstitute-ide.net'  # Replace with your actual domain
+
+
 CORS_ALLOWED_ORIGINS = [
     'https://3000-leighallend-productivey-5rpfnq7ldhc.ws.codeinstitute-ide.net',
     'https://productive-you-api-d9afbaf8a80b.herokuapp.com',
