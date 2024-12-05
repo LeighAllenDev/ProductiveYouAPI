043dd0d5e03eb5d3f99dbd406e11f3a24293f198
Leigh Allen <lallenation@gmail.com>
2024-12-05 22:07:04 +0000

    fix: add session cookie domain

--- productive_you_api/settings.py
+++ productive_you_api/settings.py
@@ -113,6 +113,7 @@ CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_DOMAIN = 'productive-you-api-d9afbaf8a80b.herokuapp.com'
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_DOMAIN = 'productive-you-api-d9afbaf8a80b.herokuapp.com'
 
 CORS_ALLOWED_ORIGINS = [
     'https://productive-you-api-d9afbaf8a80b.herokuapp.com',
