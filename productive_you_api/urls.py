354e10093e271aa17e0c658d162422f47225b057
Leigh Allen <lallenation@gmail.com>
2024-12-05 12:39:12 +0000

    fix: update views for logging out

--- productive_you_api/urls.py
+++ productive_you_api/urls.py
@@ -3,13 +3,14 @@ from django.urls import path, include
 from .views import root_route, logout_route
 from django.conf import settings
 from django.conf.urls.static import static
+from dj_rest_auth.views import LogoutView
 
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/logout/', LogoutView.as_view(), name='rest_logout'),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('profiles/', include('profiles.urls')),
     path('api/', include('tasks.urls')),
