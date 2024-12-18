16c8d7ca751bb277a064035632c8f7ed6f82333d
Leigh Allen <lallenation@gmail.com>
2024-12-18 21:49:11 +0000

    fix: update login views

--- productive_you_api/views.py
+++ productive_you_api/views.py
@@ -1,27 +1,37 @@
+from django.contrib.auth import authenticate
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
-@api_view()
+@csrf_exempt
+@api_view(['POST'])
 def login_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
+    username = request.POST.get('username')
+    password = request.POST.get('password')
     user = authenticate(request, username=username, password=password)
     if user is not None:
         refresh = RefreshToken.for_user(user)
         response = JsonResponse({'message': 'Login successful'})
-        response.set_cookie('my-app-auth', refresh.access_token, httponly=True, samesite='None', secure=True)
-        response.set_cookie('my-refresh-token', refresh, httponly=True, samesite='None', secure=True)
+        response.set_cookie(
+            'my-app-auth', refresh.access_token, httponly=True,
+            samesite='None', secure=request.is_secure()
+        )
+        response.set_cookie(
+            'my-refresh-token', str(refresh), httponly=True,
+            samesite='None', secure=request.is_secure()
+        )
         return response
-    else:
-        return JsonResponse({'error': 'Invalid credentials'}, status=401)
+    return JsonResponse({'error': 'Invalid credentials'}, status=401)
 
 
-
-@api_view()
+@api_view(['GET'])
 def root_route(request):
     return Response({
         "message": "Welcome to my ProductiveYou API for Code Institute PP05"
@@ -30,23 +40,7 @@ def root_route(request):
 
 @api_view(['POST'])
 def logout_route(request):
-    response = Response()
-    response.set_cookie(
-        key=JWT_AUTH_COOKIE,
-        value='',
-        httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
-    )
-    response.set_cookie(
-        key=JWT_AUTH_REFRESH_COOKIE,
-        value='',
-        httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
-    )
+    response = Response({"message": "Logged out successfully"}, status=status.HTTP_200_OK)
+    response.delete_cookie(JWT_AUTH_COOKIE)
+    response.delete_cookie(JWT_AUTH_REFRESH_COOKIE)
     return response
