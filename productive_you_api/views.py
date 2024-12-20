7e48edb5d500c53d3edf01805d8cf26e26ce089f
Leigh Allen <lallenation@gmail.com>
2024-12-20 22:12:51 +0000

    fix: update logout route

--- productive_you_api/views.py
+++ productive_you_api/views.py
@@ -41,6 +41,27 @@ def root_route(request):
 @api_view(['POST'])
 def logout_route(request):
     response = Response({"message": "Logged out successfully"}, status=status.HTTP_200_OK)
-    response.delete_cookie(JWT_AUTH_COOKIE)
-    response.delete_cookie(JWT_AUTH_REFRESH_COOKIE)
+    
+    # Clear the authentication token cookie
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',  # Expire immediately
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    
+    # Clear the refresh token cookie
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    
     return response
